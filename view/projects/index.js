04e7650234f2eeeef2a002fedab26ddd72a5b016
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-04-04 22:17:40 +0530

    view

--- view/projects/index.js
+++ view/projects/index.js
@@ -4,20 +4,33 @@ const userModels = require("../../model/userModels");
 // Add a new Project
 const addProject = async (req, res) => {
   const getUser = await userModels.findById(req.body.token);
-  const projectData = new allprojectsModels({
-    user: getUser.id,
-    title: req.body.title,
-    desc: req.body.desc,
-    deadline: req.body.deadline,
-    start: new Date(),
-    progress: req.body.progress,
-    status: req.body.status,
-  });
-  try {
-    const saveData = await projectData.save();
-    res.json(saveData);
-  } catch (err) {
-    if (err) throw err;
+  if (getUser) {
+    console.log("user", getUser);
+    const projectData = new allprojectsModels({
+      user: getUser.id,
+      title: req.body.title,
+      desc: req.body.desc,
+      deadline: req.body.deadline,
+      start: new Date(),
+      progress: req.body.progress,
+      status: req.body.status,
+    });
+    try {
+      const saveData = await projectData.save();
+      res.status(201).json({
+        title: saveData.title,
+        desc: saveData.desc,
+        deadline: saveData.deadline,
+        start: saveData.start,
+        progress: saveData.progress,
+        status: saveData.status,
+        id: saveData.id,
+      });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  } else {
+    res.status(404).json({ message: "User Not Found Kindly Login again!" });
   }
 };
 
@@ -25,8 +38,28 @@ const addProject = async (req, res) => {
 const getProjects = async (req, res) => {
   //   const getUser = await userModels.findById(req.params.id);
   //   res.json(getUser);
-  const allProjects = await allprojectsModels.find({ user: req.params.id });
-  res.json(allProjects);
+  try {
+    const allProjects = await allprojectsModels
+      .find({ user: req.params.id })
+      .sort({ _id: -1 });
+    console.log("allProjects", allProjects);
+    res.status(200).json(allProjects);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
-module.exports = { addProject, getProjects };
+// Update Project Data
+const updateproject = async (req, res) => {
+  const updateData = req.body;
+  try {
+    await allprojectsModels.findByIdAndUpdate(req.params.id, updateData);
+    allprojectsModels.findById(req.params.id).then((result) => {
+      res.status(200).json(result);
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+module.exports = { addProject, getProjects, updateproject };
--- view/user/index.js
+++ view/user/index.js
@@ -6,41 +6,51 @@ const keyLength = 64;
 const digest = "sha512";
 // New User API
 const NewUserApi = async (req, res) => {
-  const { email, username, password } = await req.body;
-  const checkUser = await userModels.findOne({ email: email });
-  if (!checkUser) {
-    // Code to Hash Password and save the data to our database
-    const salt = crypto.randomBytes(16).toString("hex");
+  console.log(req.body);
 
-    crypto.pbkdf2(
-      password,
-      salt,
-      iterations,
-      keyLength,
-      digest,
-      async (err, key) => {
-        if (err) throw err;
-        const hash = key.toString("hex");
-        console.log(email, username, password, salt, hash);
-        const newUser = new userModels({
-          email: email,
-          username: username,
-          salt: salt,
-          hash: hash,
-        });
-        // Save user Data to database
-        try {
-          const addUser = await newUser.save();
-          res.status(200).json(addUser);
-          return;
-        } catch (err) {
-          res.json({ message: err.message });
+  try {
+    const { email, username, password } = await req.body;
+    const checkUser = await userModels.findOne({ email: email });
+    if (!checkUser) {
+      // Code to Hash Password and save the data to our database
+      const salt = crypto.randomBytes(16).toString("hex");
+
+      crypto.pbkdf2(
+        password,
+        salt,
+        iterations,
+        keyLength,
+        digest,
+        async (err, key) => {
+          if (err) throw err;
+          const hash = key.toString("hex");
+          console.log(email, username, password, salt, hash);
+          const newUser = new userModels({
+            email: email,
+            username: username,
+            salt: salt,
+            hash: hash,
+          });
+          // Save user Data to database
+          try {
+            const addUser = await newUser.save();
+            res.status(201).json({
+              token: addUser.id,
+              message: "Account Created Successfully!",
+              username: addUser.username,
+            });
+            return;
+          } catch (err) {
+            res.status(400).json({ message: err.message });
+          }
         }
-      }
-    );
-  } else {
-    console.log("User already exist");
-    res.send("Email is already in use!");
+      );
+    } else {
+      // console.log("User already exist");
+      res.status(400).json({ message: "Email is already in use!" });
+    }
+  } catch (err) {
+    res.json({ message: err.message });
   }
 };
 
@@ -49,7 +59,7 @@ const loginApi = async (req, res) => {
   const { email, password } = await req.body;
   try {
     const userData = await userModels.findOne({ email: email });
-    console.log(userData);
+    // console.log(userData);
     if (userData) {
       const { salt, hash } = await userData;
       crypto.pbkdf2(
@@ -62,19 +72,41 @@ const loginApi = async (req, res) => {
           if (err) throw err;
           const hashGenerated = key.toString("hex");
           if (hash === hashGenerated) {
-            res.send("Login Successfully");
+            res.status(200).json({
+              token: userData.id,
+              username: userData.username,
+              message: "Login Successfull!",
+            });
             return;
           } else {
-            res.send("Wrong Password!");
+            res.status(401).json({ message: "Wrong Password!" });
           }
         }
       );
     } else {
-      res.send("User isn't Registered");
+      res.status(404).json({ message: "User isn't Registered" });
     }
   } catch (err) {
     res.json({ message: err.message });
   }
 };
 
-module.exports = { NewUserApi, loginApi };
+// GetUser Data API
+// const getUserData = async (req, res) => {
+//   const token = req.params.token;
+//   try {
+//     const userData = await userModels.findById(token);
+//     res.json({
+//       username: userData.username,
+//     });
+//   } catch (err) {
+//     res.json({ message: err.message });
+//   }
+// };
+
+module.exports = {
+  NewUserApi,
+
+  loginApi,
+  //  getUserData
+};
