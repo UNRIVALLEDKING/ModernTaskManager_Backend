0d69f208191498a86758165412b9b701ca787b7a
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-04-06 22:47:30 +0530

    index

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ const databaseURL = process.env.DATABASE_URL;
 const routes = require("./routes/routes");
 const port = 8080;
 
-mongoose.connect(databaseURL, { useNewUrlParser: true });
+const serverDB = async () => {
+  try {
+    const conDB = await mongoose.connect(databaseURL, {
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected : ${conDB.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 const db = mongoose.connection;
 
 db.on("open", () => {
@@ -18,6 +28,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
-app.listen(port, () => {
-  console.log(`Server Started at ${port}`);
+serverDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server Started at ${port}`);
+  });
 });
